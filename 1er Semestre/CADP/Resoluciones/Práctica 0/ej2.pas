c3481474303475b5e8bc3c647c60c85d409d1edc
SantUNLP <santiagogabriel.rojaszapata@gmail.com>
2025-04-15 16:47:22 -0300

    Delete 1er Semestre/CADP/Resoluciones directory

--- 1er Semestre/CADP/Resoluciones/Práctica 0/ej2.pas
+++
@@ -1,9 +0,0 @@
-program practica0punto2b;
-var
-    a,b:integer;
-begin
-  writeln ("ingrese un numero entero: "); readln(a); // 4
-  writeln ("ingrese un numero entero: "); readln(b); // 8
-  writeln("se ingresaron los numeros: ", a+b); //8 y 4
-end.
-
--- 1er Semestre/CADP/Resoluciones/Práctica 0/ej4a.pas
+++
@@ -1,9 +0,0 @@
-program practica0punto4a;
-var
- diametro,radio:real;
-begin
- writeln ('ingrese el diametro: '); readln(diametro);
- radio := (diametro/2);
- writeln('El radio del circulo es: ',radio:4:2);
-end.
-
