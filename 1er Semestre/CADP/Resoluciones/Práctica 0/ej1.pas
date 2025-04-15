bcdc849ad762fcafcced1e3627ab5733a187054a
SantUNLP <santiagogabriel.rojaszapata@gmail.com>
2025-04-15 16:37:05 -0300

    Add files via upload

---
+++ 1er Semestre/CADP/Resoluciones/Práctica 0/ej1.pas
@@ -0,0 +1,10 @@
+program practica0punto1;
+var
+    a,b:integer;
+begin
+  writeln ("ingrese un numero entero: "); readln(a); // 4
+  writeln ("ingrese un numero entero: "); readln(b); // 8
+  
+  writeln("se ingresaron los numeros: ",b,a); //8 y 4
+end.
+
---
+++ 1er Semestre/CADP/Resoluciones/Práctica 0/ej2.pas
@@ -0,0 +1,9 @@
+program practica0punto2b;
+var
+    a,b:integer;
+begin
+  writeln ("ingrese un numero entero: "); readln(a); // 4
+  writeln ("ingrese un numero entero: "); readln(b); // 8
+  writeln("se ingresaron los numeros: ", a+b); //8 y 4
+end.
+
